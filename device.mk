e21c127dcc80acedc328424d42d212df54c695f0
Deivid21 <david.parra.ignacio@gmail.com>
2025-03-24 01:19:40 -0300

    Revert "beckham: Use minimal GMS if building with it"
    This reverts commit aa3e518bbc64cd781924bf290f47b22025920c99.

--- device.mk
+++ device.mk
@@ -57,11 +57,6 @@ PRODUCT_PACKAGES += \
 # Dex-pre-opt exclusions
 $(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
 
-# GMS
-ifeq ($(WITH_GMS),true)
-GMS_MAKEFILE=gms_minimal.mk
-endif
-
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.sys.beckham.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.sys.beckham.rc
